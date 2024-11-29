30e593c857dd7b447598691c7b8d91cc43c79925
zcine <jpvieira.lopes2008@gmail.com>
2024-11-29 18:00:57 -0300

    Final do Site

--- assets/js/ScriptQuiz.js
+++ assets/js/ScriptQuiz.js
@@ -13,16 +13,17 @@ function advinhar() {
 
     let pagina;
     if (contAcertou == 10) {
-        pagina = 'Novato';
-    }
-    if (contAcertou > maior) {
-        pagina = 'Entusiasta';
-    }
-    if (contAcertou > maior) {
-        pagina = 'Especialista';
-    }
-    if (contAcertou > maior) {
         pagina = 'ViciadoemDoramas';
+        //Se acertou 10
+    } else if (contAcertou > 6) {
+        pagina = 'Especialista';
+        //Se acertou 7, 8, 9
+    } else if (contAcertou > 3) {
+        pagina = 'Entusiasta';
+        //Se acertou 4, 5, 6
+    } else {
+        pagina = 'Novato';
+        //Se acertou 0, 1, 2, 3
     }
 
     localStorage.setItem('Acertos', contAcertou);
